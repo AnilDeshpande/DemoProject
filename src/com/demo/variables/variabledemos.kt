3c5ca863e9dc0d48ae9709b1d7c347fb9e502cbc
Anil Deshpande <anildesh82@gmail.com>
2019-09-09 19:58:32 +0530

    another commit

--- src/com/demo/variables/variabledemos.kt
+++ src/com/demo/variables/variabledemos.kt
@@ -1,5 +1,12 @@
 package com.demo.variables
 
 fun main(args: Array<String>) {
+    var sample  = "Welcome to programming"
 
+    var million1 = 10_00_000
+
+    var million2: Int? = 1000_000
+
+    println("Testing $million1 == $million1 ${million1==million2}")
+    println("Testing $million1 === $million2 ${million1===million2}")
 }
