58ca34d2ad8182c81d5ab11193b6ce41d37511ce
Anil Deshpande <anildesh82@gmail.com>
2020-01-15 13:04:39 +0530

    committing break and continue demos

--- src/com/demo/controlflows/BreakContinues.kt
+++ src/com/demo/controlflows/BreakContinues.kt
@@ -1,8 +1,18 @@
 package com.demo.controlflows
 
 fun main(args: Array<String>) {
-    loop@ for(i in 1..10){
-        if(i==5) continue@loop
-        println(i)
+    /*outerloop@for(i in 1..3){
+        innerloop@for(j in 1..3) {
+            if(j==2) break@outerloop
+            println("i: ${i}, j: ${j} \t")
+        }
+        println()
+    }*/
+
+    //return
+    listOf(1,2,3,4,5,6).forEach sampl@{
+        if(it==3) return@sampl
+        println(it)
     }
 }
+
