dd5d5214077b5a02421de0c26dd6dac61b73e8c3
Anil Deshpande <anildesh82@gmail.com>
2021-06-07 17:16:04 +0530

    Inner classes demo added

---
+++ src/com/demo/innerclassesdemo/InnerClassesDemo.kt
@@ -0,0 +1,30 @@
+package com.demo.innerclassesdemo
+
+class Person(val name: String?,
+             var address: Address?){
+    inner class Address(val buildingNumber: String,
+                  val streetName: String,
+                  val locality: String,
+                  val city: String,
+                  val PIN: String){
+        val contextName = name
+
+        fun  displayAddress (){
+            println(" ${this.contextName} resides at ${this.streetName}, ${locality}, ${city}")
+        }
+    }
+}
+
+
+
+fun main(array: Array<String>){
+
+    val person = Person("Anil", null);
+    person.address = person.Address("231","Sample Street", "Sample Locality","Sample City","234234")
+
+    person.address?.displayAddress()
+
+
+
+
+}
