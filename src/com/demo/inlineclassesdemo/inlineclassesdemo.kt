e6dfbe800b2c27b76d48245e9a3db9c0039ac0fa
Anil Deshpande <anildesh82@gmail.com>
2022-01-03 12:48:10 +0530

    Inline class demo being committed.

---
+++ src/com/demo/inlineclassesdemo/inlineclassesdemo.kt
@@ -0,0 +1,18 @@
+
+inline class Width(val value: Long)
+inline class Height(val value: Long)
+
+class Rectangle (width: Width, height: Height){
+    private val width: Width? = width
+    private val height: Height? = height
+    fun printSpec(){
+        println("${this.width}, ${this.height}")
+    }
+}
+
+fun main(array: Array<String>){
+    val width = Width(100L)
+    val height = Height(50L)
+    var shape = Rectangle( width,height)
+    shape.printSpec()
+}
