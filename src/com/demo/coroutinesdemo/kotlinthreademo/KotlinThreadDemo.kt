3f1b3194f313b82f5fb766ab0d6e8442727900fd
Anil Deshpande <anil.deshpande@shell.com>
2023-02-28 22:13:06 +0530

    minor changes

--- src/com/demo/coroutinesdemo/kotlinthreademo/KotlinThreadDemo.kt
+++ src/com/demo/coroutinesdemo/kotlinthreademo/KotlinThreadDemo.kt
@@ -1,13 +1,13 @@
 package com.demo.coroutinesdemo.kotlinthreademo
 
-internal class MyThread : Thread() {
+internal class MyThread(val threadName: String, val sleepDelay: Long) : Thread(threadName) {
     override fun run() {
         println("Worker Thread Started")
         println("Thread name: " + currentThread().name + ", Thread id: " + currentThread().id)
         for (i in 0..9) {
             try {
-                sleep(1000)
-                println("Remaining time left: " + (10 - i))
+                sleep(sleepDelay)
+                println("Remaining time left for ${Thread.currentThread().name}: " + (10 - i))
             } catch (e: InterruptedException) {
                 e.printStackTrace()
             }
@@ -21,6 +21,6 @@ internal class MyThread : Thread() {
 fun main(args: Array<String>) {
     println("Main started")
     println("Thread name: " + Thread.currentThread().name + ", Thread id: " + Thread.currentThread().id)
-    MyThread().start()
+    MyThread("Thread1", 1000).start()
     println("Main ended")
 }
