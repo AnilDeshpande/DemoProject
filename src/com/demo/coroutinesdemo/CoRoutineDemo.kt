82472f0727c0c212c56b8f82587ddf09ad4534fd
Anil Deshpande <anil.deshpande@shell.com>
2023-03-11 23:09:27 +0530

    Multi Threaded execution of coroutines

--- src/com/demo/coroutinesdemo/CoRoutineDemo.kt
+++ src/com/demo/coroutinesdemo/CoRoutineDemo.kt
@@ -1,25 +1,23 @@
 package com.demo.coroutinesdemo
 
-import kotlinx.coroutines.async
-import  kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 suspend fun longRunningWork(coroutineName: String, delay: Long) {
     println("Worker Thread Started")
     println("Thread name: " + Thread.currentThread().name + ", Thread id: ${Thread.currentThread().id} in ${coroutineName}" )
     for( i in 0..9){
        delay(delay)
-        println("Remaining time left for ${Thread.currentThread().name}: " + (10 - i) +" in "+coroutineName)
+        withContext(Dispatchers.Default){
+            println("Remaining time left for ${Thread.currentThread().name}: " + (10 - i) +" in "+coroutineName)
+        }
     }
-    println("Worker Thread ended")
+    println("${coroutineName} Thread ended")
 }
 
  fun main() = runBlocking {
     println("Main Started")
     println("Thread name: " + Thread.currentThread().name + ", Thread id: " + Thread.currentThread().id)
-     for(i in 0..1000000) {
-         async { longRunningWork("CoRoutine ${i}", 100*i.toLong()) }
-     }
-
+     async { longRunningWork("CoRoutine1", 500) }
+     async { longRunningWork("CoRoutine2", 400) }
     println("Main ended")
 }
